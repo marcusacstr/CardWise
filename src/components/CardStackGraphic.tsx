d202eba328026b628aca0b89283e87a288014c4a
marcusacstr <marcus.acaster@gmail.com>
2025-06-23 08:14:08 -0700

    Fix CardStackGraphic to be responsive instead of fixed width

--- src/components/CardStackGraphic.tsx
+++ src/components/CardStackGraphic.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 
 const CardStackGraphic = () => {
   return (
-    <div className="relative w-64 h-64">
+    <div className="relative w-full aspect-square max-w-sm mx-auto">
       {/* Card 1 (Bottom) */}
-      <div className="absolute bottom-0 left-0 w-48 h-32 bg-green-700 rounded-lg transform rotate-6">
+      <div className="absolute bottom-0 left-0 w-3/4 h-1/2 bg-green-700 rounded-lg transform rotate-6">
       </div>
       {/* Card 2 */}
-      <div className="absolute bottom-8 left-8 w-48 h-32 bg-gray-400 rounded-lg transform rotate-3">
+      <div className="absolute bottom-8 left-8 w-3/4 h-1/2 bg-gray-400 rounded-lg transform rotate-3">
       </div>
       {/* Card 3 */}
-      <div className="absolute bottom-16 left-16 w-48 h-32 bg-gray-200 rounded-lg transform rotate-1">
+      <div className="absolute bottom-16 left-16 w-3/4 h-1/2 bg-gray-200 rounded-lg transform rotate-1">
       </div>
       {/* Card 4 (Top) */}
-      <div className="absolute bottom-24 left-24 w-48 h-32 bg-gradient-to-br from-green-600 to-green-800 rounded-lg">
+      <div className="absolute bottom-24 left-24 w-3/4 h-1/2 bg-gradient-to-br from-green-600 to-green-800 rounded-lg">
         <div className="absolute top-4 left-4 w-8 h-6 bg-gray-300 rounded-sm"></div>
         <div className="absolute top-6 right-4 text-white text-sm font-semibold">CardWise</div>
         <div className="absolute bottom-4 right-4 text-white text-2xl">)))</div>
